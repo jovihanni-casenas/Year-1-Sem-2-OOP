db1357556dbbb31cbc0aaf9eefe4d4a212dbb0ce
jovihanni-casenas <92214389+jovihanni-casenas@users.noreply.github.com>
2022-04-10 14:33:21 +0800

    Add files via upload

---
+++ appendicitis.h
@@ -0,0 +1,9 @@
+#ifndef APPENDICITIS_H
+#define APPENDICITIS_H
+class App{
+	public:
+		void input();
+		void breakString();
+		void appendMore();
+};
+#endif
---
+++ quiz 3 batch 1 with header file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <algorithm>
+#include "appendicitis.h"
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	App ap;
+	ap.input();
+	ap.breakString();
+	return 0;
+}
